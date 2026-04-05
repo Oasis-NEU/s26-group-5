c60e788b147f67c8ec4faf62250605e010fbd2a8
tylert51 <tylertang50@gmail.com>
2026-04-05 19:28:26 -0400

    edited main

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import Trade from './trade.jsx'
+import Trade from './Trade.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
